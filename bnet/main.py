58382c429b6114a33e634866e0f0bfee38b6f2eb
Maneet <dev.maneet@gmail.com>
2024-12-13 15:56:13 +0400

    private code

--- bnet/main.py
+++
@@ -1,6 +0,0 @@
-"""FastAPI app creation, logger configuration and main API routes."""
-
-from bnet.di import global_injector
-from bnet.launcher import create_app
-
-app = create_app(global_injector)
